b34a6952429c789b91aa79ae3a6b38f4611314d1
unknown <ssahasrabhojane@gmail.com>
2020-06-21 14:11:46 +0530

    feat: Add Static Navigation and Path Map with Desktop view.
    - feat: Add Navigation Component.
    - style: User Section, User Response List, User Response.
    - feat: Add Static PathMap.
    - style: static PathMap for desktop view.

--- src/components/UserResponseList.jsx
+++ src/components/UserResponseList.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import '../styles/index.css';
 import { UserResponse } from './UserResponse';
-import data from '../data/test-map.json';
+import json from '../data/test-map.json';
+import { useState } from 'react';
 
 //List of the User Responses in User Section
 export function UserResponseList() {
+  const [data, setData] = useState(json);
+
+  function NavigateChild(child) {
+    if (child !== undefined) {
+      setData(child);
+    }
+  }
+
   return (
     <div>
-      {data.map((d, index) => (
-        <UserResponse name={d.name} index={index + 1} />
+      {Object.entries(data).map((d, index) => (
+        <UserResponse
+          key={d[0] + index}
+          object={d[1]}
+          index={index + 1}
+          setChild={(child) => NavigateChild(child)}
+        />
       ))}
     </div>
   );
