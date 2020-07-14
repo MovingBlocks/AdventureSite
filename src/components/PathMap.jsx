ad0fe301b98d70f70982be8c40595a2b4c3785dc
Prathamesh Sahasrabhojane <ssahasrabhojane@gmail.com>
2020-07-15 02:29:44 +0530

    fix: public folder and feat: add basic animations (#10)
    * Remove public folder from .gitignore
    
    * Update Public folder
    
    * feat: Add basic animations
    
    * fix: animation delay

--- src/components/PathMap.jsx
+++ src/components/PathMap.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function PathMap() {
   return (
-    <div>
+    <div className={"fade-in-to-right"}>
       <center>
         <svg width='100%' className='svg'>
           <a
--- src/components/UserMessageSection.jsx
+++ src/components/UserMessageSection.jsx
@@ -2,21 +2,14 @@ import React from 'react';
 import PathMap from './PathMap';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
 import UserMessage from './UserMessage';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export function UserMessageSection({ setjsonPathMap, jsonPathMap }) {
   return (
-    <div>
-      <div>
-        <center>
-          <div className='tip'>Tip : Choose one of the following options.</div>
-        </center>
-      </div>
+    <div className={"fade-in-to-right"}>
       <Router>
         <Route
           render={({ location }) => (
-            <TransitionGroup>
-              <CSSTransition key={location.key} timeout={650} classNames='fade'>
+
                 <Switch location={location}>
                   <Route
                     path='/'
@@ -31,8 +24,6 @@ export function UserMessageSection({ setjsonPathMap, jsonPathMap }) {
 
                   <Route path='/map' component={PathMap} />
                 </Switch>
-              </CSSTransition>
-            </TransitionGroup>
           )}
         />
       </Router>
