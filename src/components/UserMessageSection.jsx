d5a407623142ca031294679addafb0b80d397f57
Pratham <ssahasrabhojane@gmail.com>
2020-07-26 15:03:02 +0530

    feat: Add URL Access to intermediate stage.

--- src/components/UserMessageSection.jsx
+++ src/components/UserMessageSection.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PathMap from './PathMap';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
 import UserMessage from './UserMessage';
 
 export function UserMessageSection({ setjsonPathMap, jsonPathMap }) {
   return (
-    <div className={'fade-in-to-right'}>
-      <Router>
-        <Route
-          render={({ location }) => (
-            <Switch location={location}>
-              <Route
-                path='/'
-                exact
-                component={() => (
+    <Router>
+      <Route
+        render={({ location }) => (
+          <Switch location={location}>
+            <Route
+              // pathId - URL param for directly accessing particular path.
+              path='/:pathId?'
+              component={({ match }) => (
+                <div className={'fade-in-to-right'}>
                   <UserMessage
+                    pathId={match.params.pathId}
                     setjsonPathMap={(json) => setjsonPathMap(json)}
                     jsonPathMap={jsonPathMap}
                   />
-                )}
-              />
+                </div>
+              )}
+            />
 
-              <Route path='/map' component={PathMap} />
-            </Switch>
-          )}
-        />
-      </Router>
-    </div>
+            <Route path='/map' component={PathMap} />
+          </Switch>
+        )}
+      />
+    </Router>
   );
 }
