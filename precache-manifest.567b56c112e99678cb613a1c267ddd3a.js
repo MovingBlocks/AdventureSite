a905bc359b928cb1bde91405e6d90a12af358907
jdrueckert <jd.rueckert@googlemail.com>
2020-10-07 13:35:44 +0000

    Deploying to gh-pages from  @ e9425f2bb220da976288a2e95fe7bd3e1378709b 🚀

---
+++ precache-manifest.567b56c112e99678cb613a1c267ddd3a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfaa3fcd03ba87feacf974b1b3e41d4",
+    "url": "/AdventureSite/index.html"
+  },
+  {
+    "revision": "fa64533de1588ff3dd39",
+    "url": "/AdventureSite/static/css/main.29c6d23f.chunk.css"
+  },
+  {
+    "revision": "a4e274ae1eae56d30a6d",
+    "url": "/AdventureSite/static/js/2.92ba811b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AdventureSite/static/js/2.92ba811b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa64533de1588ff3dd39",
+    "url": "/AdventureSite/static/js/main.ef08562e.chunk.js"
+  },
+  {
+    "revision": "56d40c3dd4ec3ebeebb6",
+    "url": "/AdventureSite/static/js/runtime-main.870d55b4.js"
+  },
+  {
+    "revision": "6eeb0fecafb0cad7a839164e4f9722bb",
+    "url": "/AdventureSite/static/media/back.6eeb0fec.png"
+  },
+  {
+    "revision": "fbc6997a3a14c607bc28ab319eb92547",
+    "url": "/AdventureSite/static/media/gooey.fbc6997a.png"
+  },
+  {
+    "revision": "7857cab8c78e2f76491a05c5a4773896",
+    "url": "/AdventureSite/static/media/logo.7857cab8.png"
+  },
+  {
+    "revision": "2d3cbffb1c121fa94f0def2ef5100c7f",
+    "url": "/AdventureSite/static/media/ss_6.2d3cbffb.jpg"
+  },
+  {
+    "revision": "1bc6ff0a00a37b9c0569f8e0db1cbd56",
+    "url": "/AdventureSite/static/media/user-img.1bc6ff0a.png"
+  }
+]);
